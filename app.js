498470b8dfb1baa74bbde4b0774f7a8a8305af46
Selom Amuzu <selomka1@gmail.com>
2025-08-01 03:13:16 +0000

    Third commit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const button = document.querySelector(".nav-toggle");
 const links = document.querySelector('.links');
+const main = document.querySelector("main")
 
 button.addEventListener("click", () => {
     links.classList.toggle("show")
+})
+
+main.addEventListener("click", () => {
+    links.classList.remove("show")
 })
